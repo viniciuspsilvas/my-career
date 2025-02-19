2978e01d26c6e564d41d28ca8ad7c43274da2266
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-02-19 17:00:05 +1000

    Fix models

--- src/models/PersonalInfo.ts
+++ src/models/PersonalInfo.ts
@@ -1,14 +1,20 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IPersonalInfo extends Document {
-  label: string;
-  value: string;
+  fullName: string;
+  title: string;
+  email: string;
+  location: string;
+  profile: string;
 }
 
 const PersonalInfoSchema = new Schema<IPersonalInfo>(
   {
-    label: { type: String, required: true },
-    value: { type: String, required: true },
+    fullName: { type: String, required: true },
+    title: { type: String, required: true },
+    email: { type: String, required: true },
+    location: { type: String, required: true },
+    profile: { type: String, required: true },
   },
   { timestamps: true }
 );
