12caa9ee928ba48c6717608e6f09b769c5f17e6f
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-02-19 17:41:04 +1000

    Clean up the project

--- src/lib/fetchPosts.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchPosts() {
-    const response = await fetch('/api/blog');
-    const data = await response.json();
-    return data;
-  }
--- src/lib/fetchProjects.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchProjects() {
-    const response = await fetch('/api/projects');
-    const data = await response.json();
-    return data;
-  }
