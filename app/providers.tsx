3c4db6ade736628ba0a09a30d138a0f0d6ec2800
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-02-19 10:26:40 +1000

    Setup react-query

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { ThemeProvider } from "@/src/context/ThemeContext";
 import { store } from "@/src/redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 import { Provider } from "react-redux";
 
 type ProvidersProps = {
@@ -9,11 +11,15 @@ type ProvidersProps = {
 };
 
 export function Providers({ children }: ProvidersProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <Provider store={store}>
-      <ThemeProvider>
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
     </Provider>
   );
 }
