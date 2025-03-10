c656bb21a6d60336a5ad7e1dfa53ce0feb8f9ac2
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-03-10 21:34:15 +1000

    Fix route

--- app/api/flashcards/route.ts
+++ app/api/flashcards/route.ts
@@ -7,40 +7,71 @@ export async function GET() {
     await connectDB();
     const flashCard = await FlashCard.find();
     if (!flashCard) {
-      return NextResponse.json({ error: "Flashcard not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Flashcard not found" },
+        { status: 404 }
+      );
     }
     return NextResponse.json(flashCard);
   } catch (error) {
     console.error("Error fetching flashcard:", error);
-    return NextResponse.json({ error: "Error fetching flashcard" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error fetching flashcard" },
+      { status: 500 }
+    );
   }
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
     await connectDB();
-    const body = await req.json();
-    const updatedFlashCard = await FlashCard.findByIdAndUpdate(params.id, body, { new: true });
+    const body = await request.json();
+    const { id } = await params;
+
+    const updatedFlashCard = await FlashCard.findByIdAndUpdate(id, body, {
+      new: true
+    });
     if (!updatedFlashCard) {
-      return NextResponse.json({ error: "Flashcard not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Flashcard not found" },
+        { status: 404 }
+      );
     }
     return NextResponse.json(updatedFlashCard);
   } catch (error) {
     console.error("Error updating flashcard:", error);
-    return NextResponse.json({ error: "Error updating flashcard" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error updating flashcard" },
+      { status: 500 }
+    );
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+
+  const { id } = await params;
+
   try {
     await connectDB();
-    const deletedFlashCard = await FlashCard.findByIdAndDelete(params.id);
+    const deletedFlashCard = await FlashCard.findByIdAndDelete(id);
     if (!deletedFlashCard) {
-      return NextResponse.json({ error: "Flashcard not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Flashcard not found" },
+        { status: 404 }
+      );
     }
     return NextResponse.json({ message: "Flashcard deleted successfully" });
   } catch (error) {
     console.error("Error deleting flashcard:", error);
-    return NextResponse.json({ error: "Error deleting flashcard" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error deleting flashcard" },
+      { status: 500 }
+    );
   }
 }
